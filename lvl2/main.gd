c39af52e079388556c52cad65bab3e31202cf8b3
LGrifyee <s22120152@alumnos.itsur.edu.mx>
2025-10-03 09:42:57 -0600

    Fix partially pinguinDuck

--- lvl2/main.gd
+++ lvl2/main.gd
@@ -1,12 +1,22 @@
 extends Node2D
-
+var pinguin = preload("res://lvl2/pinguin_duck.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$UI_state_machine.set_state($UI_state_machine.GameState.PLAYING)
 	
+	var pinguinInstance = pinguin.instantiate()
+	pinguinInstance.position = Vector2(1800,0)
+	add_child(pinguinInstance)
+	var pinguinInstance2 = pinguin.instantiate()
+	pinguinInstance2.position = Vector2(1600,0)
+	add_child(pinguinInstance2)
+	var pinguinInstance3 = pinguin.instantiate()
+	pinguinInstance3.position = Vector2(2000,-200)
+	add_child(pinguinInstance3)
+	
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
--- lvl2/pinguin_duck.gd
+++ lvl2/pinguin_duck.gd
@@ -1,15 +1,62 @@
 extends CharacterBody2D
 
+var s_directionV = Vector2(-12,8)
+var directionV 
+#var rotateG = deg_to_rad(-45)
+var rotateG
+var onTimerStarted = false
+var pos
+var left_to_right = false
+
+enum GameState {idle, moving}
+var current_state = null
 
 
-var directionV = Vector2(-8,6)
-var rotateG = deg_to_rad(-45)
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var scSize = Vector2(ProjectSettings.get_setting("display/window/size/viewport_width"),
+ProjectSettings.get_setting("display/window/size/viewport_height") )
 
 func _ready():
+	directionV = s_directionV
+	rotateG = directionV.angle() - deg_to_rad(180)
+	
 	$AnimatedSprite2D.play("idle")
 	rotation = rotateG
-func _physics_process(delta):
+	pos = position
 	
+func _physics_process(delta):
+	# aqui se controla cuando sale de pantalla
+	if(!onTimerStarted and  (position.y>scSize.y || position.y<0 )):
+		print("bandera")
+		$restartTimer.start()
+		onTimerStarted = true
+
 	move_and_collide(directionV)
+	
+func set_state(new_state):
+	current_state = new_state
+	
+func _on_restart_timer_timeout():
+	$restartTimer.stop()
+	onTimerStarted = false
+	if(position.y<scSize.y):
+		print("taco")
+		position = pos
+		directionV = s_directionV
+		rotation = rotateG
+		rotateG = directionV.angle() - deg_to_rad(180)
+		$AnimatedSprite2D.play("idle")
+		$AnimatedSprite2D.flip_h=true
+	
+	elif(position.y>0):
+		print("perro")
+		if !left_to_right:
+			directionV = Vector2(-s_directionV.x,-s_directionV.y)
+		position = Vector2(pos.x-1400,scSize.y)
+		rotateG = directionV.angle() - deg_to_rad(180)
+		$AnimatedSprite2D.play("flying")
+		$AnimatedSprite2D.flip_v = true
+		if $AnimatedSprite2D.flip_h:
+			$AnimatedSprite2D.flip_h = false
+		rotation = rotateG
+		
+	
